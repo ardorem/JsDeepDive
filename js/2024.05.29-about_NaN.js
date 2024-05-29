9c0e06aab62e72153cf4f0bc0a67d0d7b2ddc947
MoonInKi <moonpop6@gmail.com>
2024-05-29 19:12:18 +0900

    NaN test

---
+++ js/2024.05.29-about_NaN.js
@@ -0,0 +1,9 @@
+let num = 11;
+let wrongNum = 'ee';
+console.log(typeof wrongNum)
+console.log(typeof wrongNum % 10)
+
+console.log(isNaN(wrongNum % 10))
+console.log(Number.isNaN(0 / 0))
+
+console.log(wrongNum / 0 + 1)
